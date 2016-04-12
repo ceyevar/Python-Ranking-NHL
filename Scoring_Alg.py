17f38fcfb49563b9d27e1cd1a77d5772d1df4544
Courtney Ericson <ceyevar@neo.tamu.edu>
2016-04-11 23:04:48 -0500

    Fixed Core Alg scoring

--- Scoring_Alg.py
+++ Scoring_Alg.py
@@ -1,27 +1,44 @@
 import json
 import re
 from stat_collection import import_data
-
+from collections import OrderedDict
+import helpers
 
 def main():
     import_data()
+    player_dict = {}
 
     f = open('./stats.json', 'r')
     json_data = f.read()
 
     database = json.loads(json_data)
+    del database['Leagues'][0]
+
+    for index in database['Leagues']:
+        for team in index['Teams']:
+            for player in team['Players']:
+                if 'Draft Year' in player:
+                    if(int(player['Draft Year']) != 2016):
+                        del player
+
     stats = ['ES Primary Points/GP', 'ES GF%Rel', 'ES GA']
     stats_n = {}
     for stat_num in stats:
         min_max_vals = normalize(stat_num, database)
         stats_n[stat_num] = min_max_vals
 
-    player_dict = {}
     for index in database['Leagues']:
         for team in index['Teams']:
             for player in team['Players']:
-                for stat in player:
-                    print ""
+                if(int(player['GP']) > 40 and int(player['Draft Year']) == 2016):
+                    player_dict[player['Name']] = 0
+                    stat_dict = {}
+                    for stat in stats:
+                            stat_dict[stat] = (float(player[stat])-float(stats_n[stat][0]))/(float(stats_n[stat][1])-float(stats_n[stat][0]))
+                    player_dict[player['Name']] = stat_dict['ES Primary Points/GP'] + 0.5 * stat_dict['ES GF%Rel'] + 0.25 * stat_dict['ES GA']
+    od = OrderedDict(sorted(player_dict.items(), key=lambda t: t[1]))
+    player_dict_ordered = OrderedDict(reversed(list(od.items())))
+    print player_dict_ordered
 
 def normalize(stat, database):
     min = 0
